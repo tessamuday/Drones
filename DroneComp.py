b0a7d70dfbbfc413972ca6c143071f67fe161abd
tessamuday <02tessa.muday@gulllakecs.org>
2020-11-13 13:30:05 -0500

    Initial Drone Competition File

---
+++ DroneComp.py
@@ -0,0 +1,89 @@
+# Started from Tello Template
+# This Python app is in the Public domain
+# Some parts from Tello3.py
+
+import threading, socket, sys, time, subprocess
+
+
+# GLOBAL VARIABLES DECLARED HERE....
+host = ''
+port = 9000
+locaddr = (host,port)
+tello_address = ('192.168.10.1', 8889) # Get the Tello drone's address
+
+
+
+# Creates a UDP socketd
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(locaddr)
+
+
+def recv():
+    count = 0
+    while True:
+        try:
+            data, server = sock.recvfrom(1518)
+            print(data.decode(encoding="utf-8"))
+        except Exception:
+            print ('\n****Keep Eye on Drone****\n')
+            break
+
+
+def sendmsg(msg, sleep = 6):
+    print("Sending: " + msg)
+    msg = msg.encode(encoding="utf-8")
+    sock.sendto(msg, tello_address)
+    time.sleep(sleep)
+
+# recvThread create
+recvThread = threading.Thread(target=recv)
+recvThread.start()
+
+
+# CREATE FUNCTIONS HERE....
+
+# Drones mission through the first Hula Hoop
+def firstHoop():
+    
+
+# Drones mission through the second Hula Hoop
+def secondHoop():
+
+# Drones mission through the third Hula Hoop with a yaw
+def thirdHoop():
+
+# Drones mission through the fourth Hula Hoop
+def fourthHoop():
+
+
+print("\nTessa Muday")
+print("Program Name: Drone Competition ")
+print("Date: 11.13.2020 ")
+print("\n****CHECK YOUR TELLO WIFI ADDRESS****")
+print("\n****CHECK SURROUNDING AREA BEFORE FLIGHT****")
+ready = input('\nAre you ready to take flight: ')
+
+
+try:
+    if ready.lower() == 'yes':
+        print("\nStarting Drone!\n")
+
+        sendmsg('command')
+        sendmsg('takeoff', 8)
+
+        firstHoop()
+
+        
+
+        sendmsg('land')
+
+        print('\nGreat Flight!!!')
+
+    else:
+        print('\nMake sure you check WIFI, surroundings, co-pilot is ready, re-run program\n')
+except KeyboardInterrupt:
+    sendmsg('emergency')
+
+breakr = True
+sock.close()
